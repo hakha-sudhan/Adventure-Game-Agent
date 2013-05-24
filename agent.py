26dd12cae487f02949436f8e9f9dae5f4f58fd50
Louis Tiao <louistiao@gmail.com>
2013-05-25 03:25:44 +1000

    using file handles and in place array rotation

--- agent.py
+++ agent.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import socket, sys
+import socket, sys, optparse
 
 # Constants
 GLOBAL_MAX_WIDTH = 20
@@ -35,42 +35,39 @@ def rotate_right(view):
 def rotate_left(view):
     return map(list, zip(*view)[::-1])
 
+
 class State:
+    """
+    Instances of this class are mutable objects encapsulating the state of the game. 
+    
+    State object only mutated when fed a new view and also if apply_action() returns True 
+    """
+
     def __init__(self):
         self.global_map = [['?' for j in range(2*GLOBAL_MAX_WIDTH)] for i in range(2*GLOBAL_MAX_LENGTH)]
         self.action_history = []
-        self.view_history = []
         self.row, self.col = GLOBAL_MAX_LENGTH, GLOBAL_MAX_WIDTH
         self.orientation = NORTH
         self.tools = {
             'a': 0,
             'k': 0,
             'g': 0,
-            'd': 0
+            'd': 0,
         }
 
     def __str__(self):
-        strn = ''
-        for row in self.global_map:
-            for ch in row:
-                strn += str(ch)
-            strn += '\n'
-        strn += str((self.row, self.col))
-        strn += agent_symbols[self.orientation]
-        return strn
-        
-    def __update_orientation(self, action):
-        if action == 'l':
-            self.orientation = (self.orientation - 1) % 4
-        elif action == 'r':
-            self.orientation = (self.orientation + 1) % 4
-
-    def __view_changed(self, view):
-        try:
-            last_view = self.view_history[-1]
-        except IndexError:
-            last_view = []
-        return (last_view != view)
+        result = []
+        # Agent's view of the map
+        # TODO: Print only the part of the map we have knowledge of, not the entire map 
+        result.append('\n'.join([''.join(row) for row in self.global_map]))
+        # Agent's position (note deliberate use of tuple)
+        result.append('Position: {pos}'.format(pos=(self.row, self.col)))
+        # Agent's orientation (Positional tuple created on the fly)
+        result.append('Orientation: {orient}'.format(orient=('N', 'E', 'S', 'W')[self.orientation]))
+        # Tools in the agent's arsenal
+        result.append('Aresenal: {{Axe: {a}, Key: {k}, Gold: {g}, Dynamite: {d}}}'.format(**self.tools))
+        result.append('Moves: {num_moves}'.format(num_moves=len(self.action_history)))
+        return '\n'.join(result)
     
     def __normalize_view(self, view):
         # Normalize view buffer
@@ -109,7 +106,7 @@ class State:
                     return False
                 
                 self.row, self.col = new_row, new_col
-                if cell_ahead in ['a', 'k', 'g', 'd']:
+                if cell_ahead in self.tools.keys():
                     self.tools[cell_ahead] += 1
                 return True    
                 
@@ -125,55 +122,75 @@ class State:
                     return True
             return False
 
-    def update_map(self, view):
-        self.view_history.append(view)
-        new_view = self.__normalize_view(view)
+    def update_map(self, f):
+        r = c = 0
+        for i in range(-2, 3):
+            for j in range(-2, 3):
+                if self.orientation == NORTH:
+                    r, c = self.row+i, self.col+j
+                elif self.orientation == EAST:
+                    r, c = self.row+j, self.col-i
+                elif self.orientation == SOUTH:
+                    r, c = self.row-i, self.col-j
+                elif self.orientation == WEST:
+                    r, c = self.row-j, self.col+i
+
+                if (i == 0 and j == 0):
+                    ch = agent_symbols[self.orientation]
+                else:
+                    ch = f.read(1)
+
+                self.global_map[r][c] = str(ch)
+        
+        # view = [[0 for j in range(MAX_WIDTH)] for i in range(MAX_LENGTH)]
+        # for i in range(MAX_LENGTH):
+        #     for j in range(MAX_WIDTH):
+        #         if not (i == MAX_LENGTH/2 and j == MAX_WIDTH/2):
+        #             ch = f.read(1)
+        #             view[i][j] = str(ch)
+        # 
+        # new_view = self.__normalize_view(view)
         # Update global map
-        for i in range(MAX_LENGTH):
-            for j in range(MAX_WIDTH):
-                self.global_map[self.row+i-2][self.col+j-2] = new_view[i][j]
-        self.global_map[self.row][self.col] = agent_symbols[self.orientation]
+        # for i in range(MAX_LENGTH):
+        #     for j in range(MAX_WIDTH):
+        #         self.global_map[self.row+i-2][self.col+j-2] = new_view[i][j]
+        # self.global_map[self.row][self.col] = agent_symbols[self.orientation]
+        # #f.close()
 
 def main():
     # Get and process command line arguments. 
-    # TODO: Would really like to use 'optparse' if permitted.
-    try:
-        port = int(sys.argv[1])
-    except (IndexError, TypeError):
-        print 'Usage: {program_name} <port>'.format(program_name=sys.argv[0])
-        return 1
+    parser = optparse.OptionParser(usage='usage: %prog [options]', version='%prog version 0.1')
+    parser.add_option('-p', '--port', action="store", type="int", dest="port", help='server port number to bind to (between 1025 and 65535)')
+    options, args = parser.parse_args()
 
-    # Create a TCP/IP socket
+    # Make port a "required option"
+    if not options.port:
+        parser.print_help()
+        return 1
+    
+    # Create a TCP socket
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        s.connect(('localhost', port))
+        s.connect(('localhost', options.port))
     except socket.error:
-        print 'Could not bind to port: {0}'.format(port)
+        print 'Could not bind to port: {0}'.format(options.port)
         return 1
 
     action_string = ''
+    f = s.makefile('r', MAX_DIM)
     state = State()
     while True:
-        # View buffer
-        view = [[0 for j in range(MAX_WIDTH)] for i in range(MAX_LENGTH)]
-        for i in range(MAX_LENGTH):
-            for j in range(MAX_WIDTH):
-                if not (i == MAX_LENGTH/2 and j == MAX_WIDTH/2):
-                    ch = s.recv(1)
-                    view[i][j] = str(ch)
-        simple_print(view)
-
-        state.update_map(view)
+        state.update_map(f)
         
         print state
-        
+
         action_string = raw_input('Enter Action(s): ')
         
         state.apply_action(action_string)
-        
+
         s.sendall(action_string)
 
-    sock.close()
+    s.close()
     return 0
 
 if __name__ == '__main__':
